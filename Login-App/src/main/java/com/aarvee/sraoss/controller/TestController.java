2eb79b64f6471291f0155d0c08426a016668b4cc
Naidu <Naidu@192.168.100.13>
2019-08-03 00:15:18 +0530

    First Commit

---
+++ Login-App/src/main/java/com/aarvee/sraoss/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.aarvee.sraoss.controller;
+
+public class TestController {
+
+	public static void main(String[] args) {
+		
+		String s1 = "hello";
+		System.out.println(s1);
+		
+	}
+
+}
